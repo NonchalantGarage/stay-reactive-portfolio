787fe42f2ce11a5fc353b7c3b860dc4287a5b511
curtis <hong.curtis@gmail.com>
2022-03-17 09:02:27 -0700

    set up components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Navigation';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Project from './components/Project';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+        <Nav></Nav>
+        <Header></Header>
+        <Project></Project>
+        <Footer></Footer>
+
     </div>
   );
 }
